52ada0753b550d21b385d97d51f6b4ca1c98866e
Aman Khanna <khanna14aman@gmail.com>
2021-12-13 19:54:11 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var body_parser = require('body-parser');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/users_order_information', { useNewUrlParser: true });
-
+// mongoose.connect('mongodb://localhost:27017/users_order_information', { useNewUrlParser: true });
+mongoose.connect('mongodb+srv://amankhanna:PetLoverz@cluster0.mjklf.mongodb.net/Pet_orders?retryWrites=true&w=majority')
+.then(()=>{
+  console.log("database connected");
+}).catch(err=>console.log(err));
 
 var app = express();
 
@@ -34,9 +37,16 @@ app.get("/contact", function (request, response) {
 app.post("/contact", function (req, res) {
     var myData = new information(req.body);
     myData.save().then(()=>{
-        res.send("Your order has been placed accordingly your choice")
+        // res.send("Your order has been placed accordingly your choice")
+            console.log("order saved");
+            res.sendFile(__dirname + "/views/index.html");
+       
     }).catch(()=>{
-        res.send("Please retry your given order is not placed yet");
+        // res.send("Please retry your given order is not placed yet");
+        res.send("There is some problem please try to place order again");
+        res.sendFile(__dirname + "/views/index.html");
+
+
     })
 });
 app.get("/fluffy", function (request, response) {
